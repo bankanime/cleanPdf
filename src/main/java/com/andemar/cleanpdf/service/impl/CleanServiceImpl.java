f5dd5a13b279bcd0fa6586683fa23593e78e524a
Anderson <andemartinezdev@gmail.com>
2023-02-23 22:34:43 -0500

    Upload pdf file

--- src/main/java/com/andemar/cleanpdf/service/impl/CleanServiceImpl.java
+++ src/main/java/com/andemar/cleanpdf/service/impl/CleanServiceImpl.java
@@ -1,10 +1,27 @@
 package com.andemar.cleanpdf.service.impl;
 
 import com.andemar.cleanpdf.service.CleanService;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Slf4j
 @Service
 public class CleanServiceImpl implements CleanService {
+
+    @Override
+    public void cleanFile(MultipartFile file) {
+        log.info("upload file");
+        try {
+            PdfDocument pdfDocument = new PdfDocument(new PdfReader(file.getInputStream()));
+            log.info(PdfTextExtractor.getTextFromPage(pdfDocument.getPage(8)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
